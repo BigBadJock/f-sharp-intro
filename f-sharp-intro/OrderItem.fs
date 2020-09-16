b0f3ef6ffff8357f396d8cc3d0d11b8a1a2c9bb5
John McArthur <john.mcarthur@teliqo.com>
2020-09-16 16:48:33 +0100

    Updated orderitem type, added display function

--- f-sharp-intro/OrderItem.fs
+++ f-sharp-intro/OrderItem.fs
@@ -1,6 +1,18 @@
 ﻿namespace OrderModels
 
-type OrderItem = {Name: string; Cost: decimal; Quantity: int} with
-    // member defined with type declaration
-    member this.Total = 
-        this.Cost * (decimal this.Quantity)
+module Order = 
+
+    type OrderItem = {Name: string; Cost: decimal; Quantity: int} with
+        // member defined with type declaration
+        member this.Total = 
+            this.Cost * (decimal this.Quantity)
+
+
+    let createOrderItem name cost quantity = 
+        {Name = name; Cost = cost; Quantity = quantity}
+
+    let display(orderItem: OrderItem) = 
+            printfn "%s %f %i %f" orderItem.Name orderItem.Cost orderItem.Quantity orderItem.Total
+
+    type OrderItem with 
+        member this.Display = display this
--- f-sharp-intro/Program.fs
+++ f-sharp-intro/Program.fs
@@ -20,16 +20,18 @@ let main argv =
     let squared2 = squareValues newValues
     printfn "squared newValues %A" squared2
 
-    let item1 = {Name= "Table"; Cost= 199.99m; Quantity=1 }
-    let item2 = {Name= "Chair"; Cost=  49.99m; Quantity=6 }
-    let item3 = {Name= "Lamp";  Cost=  99.99m; Quantity=1 }
-    let item4 = {Name= "Plate"; Cost=   3.99m; Quantity=6 }
-    let item5 = {Name= "Cup";   Cost=   2.99m; Quantity=6 }
+
+    let item1 = Order.createOrderItem "Table" 199.99m 1
+    let item2 = Order.createOrderItem "Chair" 49.99m 6
+    let item3 = Order.createOrderItem "Lamp" 99.99m 1
+    let item4 = Order.createOrderItem "Plate" 3.99m 6
+    let item5 = Order.createOrderItem "Cup" 2.99m 6
+
     let order = [item1; item2; item3; item4; item5]
 
-    List.iter(fun x -> printfn "%s %f %i %f" x.Name x.Cost x.Quantity x.Total) order
-
-    let orderTotal = order |> Seq.sumBy(fun (item: OrderItem) -> item.Total) 
+    List.iter( fun (item: Order.OrderItem) -> item.Display) order
+   
+    let orderTotal = order |> Seq.sumBy(fun (item: Order.OrderItem) -> item.Total) 
     printfn "order total: %f" orderTotal
 
 
